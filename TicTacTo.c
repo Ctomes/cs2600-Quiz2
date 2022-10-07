cf707c42b333dee202c25a5683deedee7d66ec24
Ctomes <72369162+Ctomes@users.noreply.github.com>
2022-10-06 18:49:02 -0700

    Commit issue 	TicTacTo

--- TicTacTo.c
+++ TicTacTo.c
@@ -2,18 +2,37 @@
 //Tomes, Christopher
 //revision v1 10/05/2022
 #include <stdio.h>
+
+//global vals:
 int remaining_pos = 9;
 char board[9] = "";
-int win_con[8] = {0};
-int winner = 0; //player 1 = 1, player = 2
+/* win condition arrays:
+Horizontal 0: 0 1 2
+           1: 3 4 5
+           2: 6 7 8
+Vertical   3: 0 3 6
+           4: 1 4 7
+           5: 2 5 8
+Diagnal    6: 0 4 8
+           7: 2 4 6 */
+static int arr[] = {0,1,2
+            ,3,4,5
+            ,6,7,8
+            ,0,3,6
+            ,1,4,7
+            ,2,5,8
+            ,0,4,8
+            ,2,4,6};
+
 
 int tryMove(int x, int y, char in)
 {
-    int board_spot = ((y-1) + (3*(x-1)));
+    int board_spot = ((y-1) + (3*(x-1))); //convert (x,y) to single array dimensionality.
 
     if(board[board_spot] == 0)
     {
-        board[board_spot] = in;
+        //set char in board and dec. remaining positions.
+        board[board_spot] = in; 
         remaining_pos--;
         return 0;
     }else
@@ -24,48 +43,27 @@ int tryMove(int x, int y, char in)
 
 int isThereAWinner()
 {
-    if(remaining_pos == 0){
-        printf("Result is a Tie!\n");
-        return 0;
-    }
-/*
-win condition arrays:
---------
-Horizontal
-0: 0 1 2
-1: 3 4 5
-2: 6 7 8
-Vertical
-3: 0 3 6
-4: 1 4 7
-5: 2 5 8
-Diagnal
-6: 0 4 8
-7: 2 4 6
---------
-//X++ O-- winner occurs if any become 3/-3
-*/
-int arr[] = {0,1,2
-            ,3,4,5
-            ,6,7,8
-            ,0,3,6
-            ,1,4,7
-            ,2,5,8
-            ,0,4,8
-            ,2,4,6};
-//0:
-for(int i = 0; i < 8; i++){
-    if(board[arr[3*i]]+board[arr[3*i+1]]+board[arr[3*i+2]] == (88*3))
+
+//Loop over arr[] rows and check board positions.
+for(int i = 0; i < 8; i++)
+{
+    if(board[arr[3*i]]+board[arr[3*i+1]]+board[arr[3*i+2]] == (88*3))// 3 X's
     {
         printf("PLAYER 1 IS THE WINNER\n");
         return 0;
-    }else if(board[arr[3*i]]+board[arr[3*i+1]]+board[arr[3*i+2]] == (79*3))
+    }else if(board[arr[3*i]]+board[arr[3*i+1]]+board[arr[3*i+2]] == (79*3)) // 3 O's
     {
         printf("PLAYER 2 IS THE WINNER\n");
         return 0;
     }
 }
+    if(remaining_pos == 0){
+        printf("Result is a Tie!\n");
+        return 0;
+    }
     return 1;
+
+
 
 }
 void printBoard()
